387306833e23df9216b1504d36f8824ff4e98bf9
Sebwebswiss <sebwebswiss@gmail.com.com>
2025-04-11 09:12:48 -0400

    mapa-contact

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -135,16 +135,17 @@ const ContactUs = () => {
 
                     {/* Google Maps Embed */}
                     <div className="mt-10">
-                        <h3 className="text-2xl font-semibold text-primary text-center">Find Us Here</h3>
-                        <div className="mt-4 w-full h-64 rounded-lg overflow-hidden">
-                            <iframe
-                                className="w-full h-full"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509529!2d144.95373631531887!3d-37.81627977975179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf0727e49f9d29a4!2sPet%20Care!5e0!3m2!1sen!2sus!4v1614128745966!5m2!1sen!2sus"
-                                allowFullScreen
-                                loading="lazy"
-                            ></iframe>
-                        </div>
-                    </div>
+  <h3 className="text-2xl font-semibold text-primary text-center">Find Us Here</h3>
+  <div className="mt-4 w-full h-64 rounded-lg overflow-hidden">
+    <iframe
+      className="w-full h-full"
+      src="https://www.google.com/maps?q=11+Plaza+Real+S,+Boca+Raton,+FL+33432&output=embed"
+      allowFullScreen
+      loading="lazy"
+    ></iframe>
+  </div>
+</div>
+
                 </section>
             </div>
         </>
