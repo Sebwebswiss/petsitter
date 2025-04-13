36cccbba7d3421d8bedf5ac280a60a880bf43925
Sebwebswiss <sebwebswiss@gmail.com.com>
2025-04-13 09:46:15 -0400

    icoonica

--- src/app/client/components/sidebar.tsx
+++ src/app/client/components/sidebar.tsx
@@ -151,19 +151,22 @@ const Sidebar = () => {
                 }`}
               >
                 <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="30"
+                  width="30"
                   viewBox="0 0 24 24"
                   fill="none"
-                  height={30}
-                  width={30}
                   stroke="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 >
-                  <path
-                    d="M3 8H21M3 16H21M3 12H21"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
+                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                  <line x1="16" y1="2" x2="16" y2="6"></line>
+                  <line x1="8" y1="2" x2="8" y2="6"></line>
+                  <line x1="3" y1="10" x2="21" y2="10"></line>
+                  <line x1="12" y1="14" x2="12" y2="20"></line>
+                  <line x1="9" y1="17" x2="15" y2="17"></line>
                 </svg>
                 <span className="md:inline">Request a Booking</span>
               </Link>
