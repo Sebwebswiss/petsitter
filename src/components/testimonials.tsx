acc97ea84ea78a1cb2df627cccb155f5646ac45b
Sebwebswiss <sebwebswiss@gmail.com.com>
2025-04-13 10:43:47 -0400

    ljudi slike test

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -22,7 +22,6 @@ const testimonials = [
     text: "I was nervous about leaving our new rescue pup for the first time, but this service was a game-changer. Sebastien is patient and so good with animals - he even managed our pup's medication and anxiety with ease. We got updates every visit. Truly a five-star experience from start to finish.",
   },
 ];
-
 
 const Testimonials = () => {
   const settings = {
@@ -56,35 +55,56 @@ const Testimonials = () => {
           <div className="lg:flex lg:items-center lg:justify-between z-10">
             <div>
               <img
-                src={"/images/testimonials/david.jpg"}
+                src={"/icons/testimonial.png"}
                 alt={`efficient icon`}
                 className="h-16 w-16 mb-6"
               />
               <h2 className="text-3xl font-extrabold text-center text-golden font-heading uppercase">
-              Testimonials & Reviews
+                Testimonials & Reviews
               </h2>
-              <p className="text-white mt-4 text-center">Don&apos;t just take our word for it - here&apos;s what some of our happy clients have to say about our pet care services</p>
+              <p className="text-white mt-4 text-center">
+                Don&apos;t just take our word for it - here&apos;s what some of
+                our happy clients have to say about our pet care services
+              </p>
             </div>
           </div>
           <div className="mt-12 w-full z-10 h-full">
             <Slider {...settings} className="">
               {testimonials.map((testimonial, index) => (
                 <div key={index} className="px-2 md:py-2 md:px-0">
-                <div className="bg-black rounded-lg shadow-md p-6 md:max-w-[350px]  md:mx-auto h-full border border-primary">
-                  <div className="flex items-center justify-center h-24 w-24 mx-auto mb-4 bg-[#343538] rounded-full">
+                  <div className="bg-black rounded-lg shadow-md p-6 md:max-w-[350px]  md:mx-auto h-full border border-primary">
+                    <div className="flex items-center justify-center h-24 w-24 mx-auto mb-4 rounded-full overflow-hidden bg-[#343538]">
+                      {testimonial.image ? (
+                        <img
+                          src={testimonial.image}
+                          alt={testimonial.name}
+                          className="h-full w-full object-cover"
+                        />
+                      ) : null}
+                    </div>
+
+                    <h3 className="text-lg font-bold text-center text-white font-heading">
+                      {testimonial.name}
+                    </h3>
+                    <p className="text-primary font-heading mb-4 text-center font-extrabold">
+                      {testimonial.location}
+                    </p>
+                    <p className="text-white text-center text-sm  md:min-h-40">
+                      {testimonial.text}
+                    </p>
                   </div>
-                  <h3 className="text-lg font-bold text-center text-white font-heading">
-                    {testimonial.name}
-                  </h3>
-                  <p className="text-primary font-heading mb-4 text-center font-extrabold">{testimonial.location}</p>
-                  <p className="text-white text-center text-sm  md:min-h-40">{testimonial.text}</p>
-                </div>
                 </div>
               ))}
             </Slider>
           </div>
-          <p className="text-white mt-2 text-center">(More testimonials coming soon - we&apos;re proud to have a growing list of satisfied pet parents!) These reviews are collected from our clients via email and other platforms, and we&apos;re happy to share references upon request. Your feedback means the world to us, and it helps us continually improve our services. We strive to ensure every experience is as positive as those mentioned above.
-</p>
+          <p className="text-white mt-2 text-center">
+            (More testimonials coming soon - we&apos;re proud to have a growing
+            list of satisfied pet parents!) These reviews are collected from our
+            clients via email and other platforms, and we&apos;re happy to share
+            references upon request. Your feedback means the world to us, and it
+            helps us continually improve our services. We strive to ensure every
+            experience is as positive as those mentioned above.
+          </p>
         </div>
       </div>
     </div>
